22f9f2f06762d024b8277edb49109f7f1a9c14dd
Gabriel Rak <grakzanchetta@gmail.com>
2022-06-21 14:52:01 -0300

    Rotas Criadas

--- index.js
+++ index.js
@@ -11,7 +11,30 @@ const app = express();
 app.use(json());
 app.use(cors());
 
+app.post("/participants", (request, response) => {
+    res.status(201).send();
+});
+
+app.get("/participants", (request, response) => {
+});
+
+app.post("/messages", (request, response) => {
+    res.status(201).send();
+});
+
+app.get("/messages", (request, response) => {
+});
+
+app.post("/status", (request, response) => {
+    res.status(200).send();
+});
+
+app.delete("/message/<id>", (request, response) => {
+});
+
+app.put("/message/<id>", (request, response) => {
+});
 
 app.listen(5000, () => {
-    console.log(chalk.bold.green("Servidor Funcionando na Porta 5000"))
+    console.log(chalk.bold.greenBright("\nServidor Funcionando na Porta 5000. \n"));
 });
