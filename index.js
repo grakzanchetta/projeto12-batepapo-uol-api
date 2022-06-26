9ff1adf4f88dd03f51a970df89002f7567162b95
Gabriel Rak <grakzanchetta@gmail.com>
2022-06-26 09:59:43 -0300

    Segundo requisito - Armazenamento - Implementado

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 import dotenv from 'dotenv'
 import express from 'express';
+import{ MongoClient } from 'mongodb';
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
+const bancoMensagens = process.env.DB_PROJETO;
+
+let db = null;
+const mongoClient = new MongoClient(process.env.MONGO_URL);
+mongoClient.connect().then(() => {
+    db = mongoClient.db(bancoMensagens)
+    console.log(`conectado com sucesso ao banco ${bancoMensagens}`);
+})    
+
 
 
 
