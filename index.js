74713385f9f370b69fcc2f112e4a772da7996745
Gabriel Rak <grakzanchetta@gmail.com>
2022-06-26 09:43:19 -0300

    Primeiro requisito - Geral - Implementado

--- index.js
+++ index.js
@@ -1,40 +1,12 @@
-import chalk from 'chalk'
-import cors from 'cors';
-import dayjs from 'dayjs'
-//import 'dotenv/config';
-import express, { json } from 'express';
-//import Joi from 'joi';
-//import { MongoClient } from 'mongodb';
-//import { CbObj, defaults, stripHtml, version } from "string-strip-html";
+import dotenv from 'dotenv'
+import express from 'express';
 
+dotenv.config();
 const app = express();
-app.use(json());
-app.use(cors());
+const port = process.env.PORT;
 
-app.post("/participants", (request, response) => {
-    res.status(201).send();
-});
 
-app.get("/participants", (request, response) => {
-});
 
-app.post("/messages", (request, response) => {
-    res.status(201).send();
-});
-
-app.get("/messages", (request, response) => {
-});
-
-app.post("/status", (request, response) => {
-    res.status(200).send();
-});
-
-app.delete("/message/<id>", (request, response) => {
-});
-
-app.put("/message/<id>", (request, response) => {
-});
-
-app.listen(5000, () => {
-    console.log(chalk.bold.greenBright("\nServidor Funcionando na Porta 5000. \n"));
-});
+app.listen(port, () => {
+    console.log(`Servidor funcionando na porta ${port}`)
+})
